48293bd93370b24292b259775eb67f2d50c901e6
Emily Harvey <emily.cm.harvey@gmail.com>
2011-07-07 14:59:27 -0600

    Added a no master authentication system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,41 @@
 class SessionsController < ApplicationController
-  def create    
+  def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
+    provider = auth["provider"]
+    uid = auth["uid"]
+    if cookies.signed[:user_id] == nil
+      # If no user is currently signed in:
+      # See if the user has signed in before. 
+      #  If not, create the user.
+      new_user = User.find_by_provider_and_uid(provider, uid) || User.create_with_omniauth(auth)
+      cookies.signed[:user_id] = new_user.id
+      
+      # Clear all historical connections since they were not 
+      #  closed properly on last close.
+      authentications = current_user.authentications.all
+      authentications.each do |auth|
+        auth.destroy
+      end
+end
+ 
+authentication = 
+    Authentication.find_by_provider_and_uid(provider, uid)
+    if authentication == nil
+      # Only create a new authentication for the user if the 
+      #  user has not signed in previously.
+      current_user.authentications.create!(:provider => provider, :uid => uid)
+    end
+    
     redirect_to root_url, :notice => "Signed in!"
-  end
-
+  end                                                           
+  
   def destroy
-    session[:user_id] = nil
+    authentications = current_user.authentications.all
+    authentications.each do |auth|
+      auth.destroy
+    end
+    
+    cookies.delete :user_id
     redirect_to root_url, :notice => "Signed out!"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110707203559) do
+ActiveRecord::Schema.define(:version => 20110707205115) do
+
+  create_table "authentications", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "identities", :force => true do |t|
     t.string   "password_digest"
