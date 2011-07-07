8e004a9ebd90839a6d4eeb7bb1b8a9226fdda1de
Emily Harvey <emily.cm.harvey@gmail.com>
2011-07-07 14:47:56 -0600

    Added twitter and github to my application

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identity, :fields => [:name, :email]
+  provider :twitter, 'L6JfNpaiOACtuaW9nP2kA', 'D8Pn2CkEXcHwrvv2lKV2fyejDkFuiOoWtABe1bQIQM'
+  provider :github, 'f6d9845597fb42f76380', '08665942800b0a952a71fda8269f9fc3b2cc8526'
 end
