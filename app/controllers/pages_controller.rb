06f062ef473483fa0b18228085f76e1a4cc6a49c
Emily Harvey <emily.cm.harvey@gmail.com>
2011-07-07 15:04:43 -0600

    Added individual disconnect functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     if current_user
       @authentications = current_user.authentications.all
+      @count = @authentications.count
     end
   end
 
