4469fdd3e5514263de6e35d112b4c203d2dc70bb
Emily Harvey <emily.cm.harvey@gmail.com>
2011-07-07 17:23:13 -0600

    Remove css links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
     if current_user
-      @authentications = current_user.authentications.all
+      #@authentications = current_user.authentications.all
+      # Tighten the datatype to work in Heroku
+      @authentications = Authentication.find :all, :conditions => ["master_id = ?", current_user.id.to_s]
       @count = @authentications.count
     end
   end
